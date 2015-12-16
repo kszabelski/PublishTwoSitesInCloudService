a5f7de90410cd6cc0b17fd781328df5ac1dfa83e
Krzysztof Szabelski <kszabelski@gmail.com>
2015-12-16 14:51:57 +0100

    Deploy two services into one

---
+++ WebRole2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebRole2.MvcApplication" Language="C#" %>
---
+++ WebRole2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebRole2.Startup))]
+namespace WebRole2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
